b5b0c5511effef29d81b3ed6059ec6b6670a0b83
Lauri Vuorela <lauri.vuorela@gmail.com>
2019-12-02 20:41:10 +0100

    updates for faster initial answers

--- base.py
+++ base.py
@@ -1,10 +1,12 @@
 import os
+from collections import *
 
 from utils import get_actual
+from utils import *
 
 
 if __name__ == "__main__":
     day = os.path.dirname(os.path.abspath(__file__)).rsplit("/", 1)[-1]
-    input_list = get_actual(day=int(day), year=2019).splitlines()
+    _input = get_actual(day=int(day), year=2019)
 
     # code here
