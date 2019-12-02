5bf60042d9408ab4afeb44155d7382e7c39fd974
Lauri Vuorela <lauri.vuorela@gmail.com>
2019-12-02 10:19:19 +0100

    default to todays date if no argument passed

--- new-day.py
+++ new-day.py
@@ -1,10 +1,17 @@
 import os
 import sys
+from datetime import date
 from shutil import copyfile
 
 
 if __name__ == "__main__":
-    day = sys.argv[1]
+    try:
+        day = sys.argv[1]
+    except Exception:
+        day = date.today().day
+        if day < 10:
+            day = f'0{day}'
+
     if os.path.isdir(day):
         sys.exit("day alreddy exists")
 
